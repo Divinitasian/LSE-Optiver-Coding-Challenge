276ca6cef2306936b57707894388f554f2bffca7
Jialin (Roka) Yi <j.yi8@lse.ac.uk>
2023-02-16 00:12:05 +0000

    Market maker class

--- market_maker.py
+++ market_maker.py
@@ -24,40 +24,8 @@ class MarketMaker:
         self.credit_bid = 0.03
         self.credit_ask = 0.03
 
-        self.volume_bid = 100
-        self.volume_ask = 100
-        
-
-class OptionMarketMaker(MarketMaker):
-    def _calculate_theoretical_option_value(self, stock_value):
-        """
-        This function calculates the current fair call or put value based on Black & Scholes assumptions.
-    
-        stock_value:             -  Assumed stock value when calculating the Black-Scholes value
-        """
-        expiry = self.primal.expiry
-        strike = self.primal.strike
-        option_kind = self.primal.option_kind
-        time_to_expiry = calculate_current_time_to_date(expiry)
-    
-        if option_kind == OptionKind.CALL:
-            option_value = call_value(S=stock_value, K=strike, T=time_to_expiry, r=self.interest_rate, sigma=self.volatility)
-        elif option_kind == OptionKind.PUT:
-            option_value = put_value(S=stock_value, K=strike, T=time_to_expiry, r=self.interest_rate, sigma=self.volatility)
-            
-        return option_value
-        
-        
-    def compute_fair_quotes(self, stock_bid_price, stock_ask_price):
-        theoretical_value_1 = self._calculate_theoretical_option_value(
-            stock_bid_price
-            )
-        theoretical_value_2 = self._calculate_theoretical_option_value(
-            stock_ask_price
-            )
-        theoretical_bid_price = min(theoretical_value_1, theoretical_value_2)
-        theoretical_ask_price = max(theoretical_value_1, theoretical_value_2)
-        return theoretical_bid_price, theoretical_ask_price
+        self.volume_bid = 10
+        self.volume_ask = 10
         
         
     def get_traded_orders(self, exchange):
@@ -67,8 +35,8 @@ class OptionMarketMaker(MarketMaker):
         trades = exchange.poll_new_trades(instrument_id=self.primal.instrument_id)
         for trade in trades:
             print(f'- Last period, traded {trade.volume} lots in {self.primal.instrument_id} at price {trade.price:.2f}, side {trade.side}.')
-        
-        
+
+
     def update_limit_orders(self, exchange, theoretical_bid_price, theoretical_ask_price):
         """
         This function updates the quotes specified by instrument id. We take the following actions in sequence:
@@ -120,6 +88,40 @@ class OptionMarketMaker(MarketMaker):
                 side='ask',
                 order_type='limit',
             )
+
+
+class OptionMarketMaker(MarketMaker):
+    def _calculate_theoretical_option_value(self, stock_value):
+        """
+        This function calculates the current fair call or put value based on Black & Scholes assumptions.
+    
+        stock_value:             -  Assumed stock value when calculating the Black-Scholes value
+        """
+        expiry = self.primal.expiry
+        strike = self.primal.strike
+        option_kind = self.primal.option_kind
+        time_to_expiry = calculate_current_time_to_date(expiry)
+    
+        if option_kind == OptionKind.CALL:
+            option_value = call_value(S=stock_value, K=strike, T=time_to_expiry, r=self.interest_rate, sigma=self.volatility)
+        elif option_kind == OptionKind.PUT:
+            option_value = put_value(S=stock_value, K=strike, T=time_to_expiry, r=self.interest_rate, sigma=self.volatility)
+            
+        return option_value
+        
+        
+    def compute_fair_quotes(self, stock_bid_price, stock_ask_price):
+        theoretical_value_1 = self._calculate_theoretical_option_value(
+            stock_bid_price
+            )
+        theoretical_value_2 = self._calculate_theoretical_option_value(
+            stock_ask_price
+            )
+        theoretical_bid_price = min(theoretical_value_1, theoretical_value_2)
+        theoretical_ask_price = max(theoretical_value_1, theoretical_value_2)
+        return theoretical_bid_price, theoretical_ask_price
+        
+        
 
 if __name__ == "__main__":
     exchange = Exchange()
