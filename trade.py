b88346d908fd7b5037dfe4b6c06da0e519c60c7e
Jialin (Roka) Yi <j.yi8@lse.ac.uk>
2023-02-16 01:48:29 +0000

    cover all instruments

--- trade.py
+++ trade.py
@@ -1,6 +1,9 @@
 import datetime as dt
 import time
 import logging
+
+from libs import get_bid_ask
+
 logging.getLogger('client').setLevel('ERROR')
 
 
@@ -35,33 +38,34 @@ def market_makers_hash(all_instruments, all_instruments_underlying_ids):
         all_market_makers[instrument_id] = market_maker
     return all_market_makers
 
-# if __name__ == "__main__":
-#     exchange = Exchange()
-#     exchange.connect()
+if __name__ == "__main__":
+    exchange = Exchange()
+    exchange.connect()
+    
+    all_instruments = exchange.get_instruments()
+    underlying_dict = underlying_hash(all_instruments)
+    market_makers_dict = market_makers_hash(all_instruments, underlying_dict)
 
+    wait_time = .2
     
-#     wait_time = 1
-    
-#     while True:
-#         print(f'')
-#         print(f'-----------------------------------------------------------------')
-#         print(f'TRADE LOOP ITERATION ENTERED AT {str(dt.datetime.now()):18s} UTC.')
-#         print(f'-----------------------------------------------------------------')
+    while True:
+        print(f'')
+        print(f'-----------------------------------------------------------------')
+        print(f'TRADE LOOP ITERATION ENTERED AT {str(dt.datetime.now()):18s} UTC.')
+        print(f'-----------------------------------------------------------------')
         
-#         all_market_makers = initialize(exchange)
+        for instrument_id, market_maker in market_makers_dict.items():
+            market_maker.get_traded_orders(exchange)
         
-#         for instrument_id, market_maker in all_market_makers.items():
-#             market_maker.get_traded_orders(exchange)
+            stock_value = get_bid_ask(exchange, underlying_dict[instrument_id])
+            if stock_value is None:
+                print('Empty stock order book on bid or ask-side, or both, unable to update option prices.')
+                time.sleep(wait_time)
+                continue
         
-#             stock_value = get_bid_ask(exchange, 'NVDA')
-#             if stock_value is None:
-#                 print('Empty stock order book on bid or ask-side, or both, unable to update option prices.')
-#                 time.sleep(wait_time)
-#                 continue
-        
-#             stock_bid, stock_ask = stock_value
-#             theoretical_bid_price, theoretical_ask_price = market_maker.compute_fair_quotes(stock_bid.price, stock_ask.price)
-#             market_maker.update_limit_orders(exchange, theoretical_bid_price, theoretical_ask_price)
+            stock_bid, stock_ask = stock_value
+            theoretical_bid_price, theoretical_ask_price = market_maker.compute_fair_quotes(stock_bid.price, stock_ask.price)
+            market_maker.update_limit_orders(exchange, theoretical_bid_price, theoretical_ask_price)
             
-#             print(f'\nSleeping for {wait_time} seconds.')
-#             time.sleep(wait_time)
+            print(f'\nSleeping for {wait_time} seconds.')
+            time.sleep(wait_time)
