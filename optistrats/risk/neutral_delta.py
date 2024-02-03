9e91a5a1fe5bf1acd7634aad7291e2e0c04fd6b2
Divinitas <jaylen.divinitas@gmail.com>
2024-02-03 22:15:21 +0000

    Sector based risk manager

--- optistrats/risk/neutral_delta.py
+++ optistrats/risk/neutral_delta.py
@@ -1,5 +1,5 @@
 """
-Manage the risk: delta and positions.
+Make the portfolio delta-neutral.
 """
 from typing import Dict
 from optibook.common_types import Instrument
@@ -7,12 +7,12 @@ from optistrats.types import PreOrder
 from optistrats.utils import MAX_BUYING_PRICE, MIN_SELLING_PRICE
 
 class RiskManager:
-    def __init__(self, base_instrument) -> None:
+    def __init__(self, base_instrument: Instrument) -> None:
         pass
 
     def action(
         self, 
-        positions: Dict[Instrument, int], 
+        related_positions: Dict[Instrument, int], 
         base_instrument_price: float
     ) -> PreOrder:
         pass
