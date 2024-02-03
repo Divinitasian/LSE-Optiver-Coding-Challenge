5cd59e52b4d5d5a33628c7dcdb5223684c74cb1a
Divinitas <jaylen.divinitas@gmail.com>
2024-02-03 18:42:00 +0000

    test for ns strategy

---
+++ optistrats/tests/strats/test_narrow_spread.py
@@ -0,0 +1,10 @@
+from optistrats.strats.narrow_spread import MarketMaker as NSMarketMaker
+
+def test_set_volume():
+    assert False
+
+def test_set_risk_premium():
+    assert False
+
+def test_action():
+    assert False
