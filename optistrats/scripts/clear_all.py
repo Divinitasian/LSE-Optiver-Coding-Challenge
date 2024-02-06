6e0043bb9f844643f78b4b92a3489855008bffb7
Divinitas <jaylen.divinitas@gmail.com>
2024-02-06 00:19:21 +0000

    clear all script

--- optistrats/scripts/clear_all.py
+++ optistrats/scripts/clear_all.py
@@ -1,11 +1,51 @@
+import time
 from optibook.synchronous_client import Exchange
-import logging
-from optistrats.utils import clear_orders, clear_position
+from optistrats.utils.constants import MIN_SELLING_PRICE, MAX_BUYING_PRICE
+
+
+def clear_position(exchange):
+    positions = exchange.get_positions()
+    pnl = exchange.get_pnl()
+    
+    print(f'Positions before: {positions}')
+    print(f'\nPnL before: {pnl:.2f}')
+    
+    print(f'\nTrading out of positions')
+    for iid, pos in positions.items():
+        if pos > 0:
+            print(f'-- Inserting sell order for {pos} lots of {iid}, with limit price {MIN_SELLING_PRICE:.2f}')
+            exchange.insert_order(iid, price=MIN_SELLING_PRICE, volume=pos, side='ask', order_type='ioc')
+        elif pos < 0:
+            print(f'-- Inserting buy order for {abs(pos)} lots of {iid}, with limit price {MAX_BUYING_PRICE:.2f}')
+            exchange.insert_order(iid, price=MAX_BUYING_PRICE, volume=-pos, side='bid', order_type='ioc')
+        else:
+            print(f'-- No initial position in {iid}, skipping..')
+        
+        time.sleep(0.10)
+    
+    time.sleep(1.0)
+    
+    positions = exchange.get_positions()
+    pnl = exchange.get_pnl()
+    print(f'\nPositions after: {positions}')
+    print(f'\nPnL after: {pnl:.2f}')
+
+
+def clear_orders(exchange):
+    for id in exchange.get_instruments():
+        orders = exchange.get_outstanding_orders(id)
+        if len(orders) == 0:
+            print(f'-- No limit order in {id}, skipping..')
+            continue
+        print(f'-- Outstanding limit orders in {id}: ')
+        for order, order_status in orders.items():
+            print(f'>>>> Deleting limit order on {order_status.side} side, with price {order_status.price} and volume {order_status.volume}.')
+        exchange.delete_orders(id)
+
 
 if __name__ == "__main__":
     exchange = Exchange()
     exchange.connect()
 
-    logging.getLogger('client').setLevel('ERROR')
     clear_orders(exchange)
     clear_position(exchange)
