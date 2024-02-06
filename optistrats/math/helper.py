4a530019225da21dba11cc7319f5cdd2e77da65d
Divinitas <jaylen.divinitas@gmail.com>
2024-02-06 23:09:03 +0000

    to ticksize

--- optistrats/math/helper.py
+++ optistrats/math/helper.py
@@ -1,69 +1,73 @@
-from typing import List
+from typing import Iterable
 from math import floor, ceil
 from optibook.common_types import PriceVolume, PriceBook
 
 
-def round_down_to_tick(price, tick_size)-> float:
-    """
-    Rounds a price down to the nearest tick, e.g. if the tick size is 0.10, a price of 0.97 will get rounded to 0.90.
-    """
-    return floor(price / tick_size) * tick_size
-
-
-def round_up_to_tick(price, tick_size)-> float:
-    """
-    Rounds a price up to the nearest tick, e.g. if the tick size is 0.10, a price of 1.34 will get rounded to 1.40.
-    """
-    return ceil(price / tick_size) * tick_size
-
-def format_order_price(
-    postive_price: float,
+def round_to_tick(
+    price: float, 
+    tick_size: float,
     side: str,
-    tick_size: float
-) -> float:
-    """Format the order price using tick size
+    to_ticksize: bool = True
+):
+    """Rounds a price down to the nearest tick.
+    
+    E.g., if the tick size is 0.10, a price of 0.97 will get rounded to 0.90 or 9.
 
     Parameters
     ----------
     price
-        the unformatted price
-    side
-        bid or ask
+        raw price in dollars
     tick_size
-        minimum price change
+        the minimum price change unit
+    side
+        bid: round down, ask: round up
+    to_ticksize, optional
+        format the price as base point in the tick size, by default True
 
     Returns
     -------
-        the formatted price. Should be multiple of tick size.
-        And bid always round down and ask always round up
+        price or base points
     """
-    assert postive_price > 0
-    if side == 'bid':
-        return round_down_to_tick(postive_price, tick_size)
-    else:
-        return round_up_to_tick(postive_price, tick_size)
+    ratio = price / tick_size
+    bsp = floor(ratio) if side == "bid" else ceil(ratio)
+    if to_ticksize:
+        return bsp
+    return  bsp * tick_size
+
+
+def get_vwap(price_volumes: Iterable[PriceVolume]) -> float:
+    """Compute the volume weighted adjusted price for a sequence of (price, volume) paris
+
+    Parameters
+    ----------
+    price_volumes
+        a sequence of price, volume
+
+    Returns
+    -------
+        the weighted average of price with the volume being the weights
+    """
+    n = 0
+    d = 0
+    for pv in price_volumes:
+        n += pv.price * pv.volume
+        d += pv.volume
+    return n/d
     
-def get_vwap(price_volume_list: List[PriceVolume]) -> float:
-    try:
-        n = 0
-        d = 0
-        for pv in price_volume_list:
-            n += pv.price * pv.volume
-            d += pv.volume
-        return n/d
-    except:
-        raise ValueError(f"price_volume_list={price_volume_list} in invalid.")
-    
+
 def get_mid_vwap(snapshot: PriceBook):
     bid_vwap = get_vwap(snapshot.bids)
     ask_vwap = get_vwap(snapshot.asks)
-    return (bid_vwap + ask_vwap) / 2
+    mid = (bid_vwap + ask_vwap) / 2
+    if mid < 0:
+        raise ValueError(f"negative mid with price book snapshot={snapshot}.")
+    return 
     
 def get_spread_vwap(snapshot: PriceBook):
     bid_vwap = get_vwap(snapshot.bids)
     ask_vwap = get_vwap(snapshot.asks)
     spread = ask_vwap - bid_vwap
-    if spread > 0:
-        return ask_vwap - bid_vwap
-    else:
+    if spread < 0:
         raise ValueError(f"negative spread with price book snapshot={snapshot}.")
+    return spread
+        
--- tests/math/test_helper.py
+++ tests/math/test_helper.py
@@ -2,19 +2,26 @@ from math import isclose
 from optibook.common_types import PriceVolume
 from optistrats.math.helper import *
 
-def test_format_order_price():
+def test_round_to_tick():
     price = 102.363
     tick_size = 0.1
-    bid_price = 102.3
-    ask_price = 102.4
+    bid_price, bid_price_in_bsp = 102.3, 1023
+    ask_price, ask_price_in_bsp = 102.4, 1024
+
+
+    assert round_to_tick(price, tick_size, 'bid') == bid_price_in_bsp
+    assert round_to_tick(price, tick_size, 'ask') == ask_price_in_bsp
+
+    # to_bsp = False
     assert isclose(
-        format_order_price(price, 'bid', tick_size),
+        round_to_tick(price, tick_size, 'bid', to_ticksize=False),
         bid_price
     ) 
     assert isclose(
-        format_order_price(price, 'ask', tick_size),
+        round_to_tick(price, tick_size, 'ask', to_ticksize=False),
         ask_price
     )
+
 
 def test_get_vwap():
     l = [
@@ -23,10 +30,3 @@ def test_get_vwap():
     ]
     ans = 8/3
     assert isclose(get_vwap(l), ans)
-    l = []
-    try:
-        get_vwap(l)
-        assert False
-    except Exception as e:
-        print(e)
-        assert True 
